097dceb84f96bcc7d54ded409e97af6bfca42a54
Marcus Renno <renno.marcus@gmail.com>
2016-03-29 20:08:06 -0400

    Fixed logic of primes. Increased performance and parallelism

--- primes.py
+++ primes.py
@@ -3,72 +3,56 @@ import numpy as np
 
 size = 10*1000*1000
 
-def eliminate_numbers(num):
-    if num%2 == 0:
-        return True
+
+
+def eliminate_number(num):
     if num%3 == 0:
         return True
     if num%5 == 0:
         return True
     return False
-    
-def is_palindrome(data):
-    if eliminate_numbers(data):
-        return False
-    str_num = str(data)
-    digits = str_num.strip()
-    size = len(digits)
-    half = size/2
-    part1 = digits[0:half]
-    if size %2 != 0:
-        part2 = digits[:half:-1]
-    else:
-        part2 = digits[:half-1:-1]
-    if part1 == part2:
-        return True
-    return False
-    
-data = []
-data1 = []
-data2 = []
-data3 = []
-data4 = []
-count = 0
-for i in range(2,  size):
-    count += 1
-    if is_palindrome(i):
-        if count == 1:
-            data1.append(i)
-        if count == 2:
-            data1.append(i)
-        if count == 3:
-            data1.append(i)
-        if count == 4:
-            data1.append(i)
-    if count == 4:
-        count = 0
+   
+data = [[] for x in range(10)]
 
-data.append(data1)
-data.append(data2)
-data.append(data3)
-data.append(data4)
+count = 0
+for i in range(7,  size, 2): #step 2 so i can eliminate all evens
+    if  not eliminate_number(i):
+        data[count].append(i)
+        count += 1
+        count = count %10
+    
 
 datasource = dict(enumerate(data))
 
-def mapfn(k, v):
+def mapfn(k, v):    
+    def is_palindrome(data):
+        str_num = str(data)
+        digits = str_num.strip()
+        size = len(digits)
+        half = size/2
+        part1 = digits[0:half]
+        if size %2 != 0:
+            part2 = digits[:half:-1]
+        else:
+            part2 = digits[:half-1:-1]
+        if part1 == part2:
+            return True
+        return False
+        
     total = len(v)
     for i in range(total):
-        number = v[i]
-        isprime = True
-        redundancy = number**0.5 #after this number repetition occurs 
-        j = 5
-        while j*j < redundancy:
-            if number % j == 0 or number % (j + 2) == 0:
-                isprime = False
-                break
-            j += 6
-        if isprime:
-            yield number,  1
+        if is_palindrome(v[i]):
+            number = v[i]
+            isprime = True
+            redundancy = number**0.5 #after this number repetition occurs 
+            j = 5
+            while j*j <= number and j <= redundancy:
+                if number % j == 0 or number % (j + 2) == 0:
+                    isprime = False
+                    break
+                j += 6
+            if isprime:
+                yield number,  1
             
 def reducefn(k, vs):
     return k
@@ -88,3 +72,5 @@ for i in results.keys():
     primes.append(i)
 
 print sorted(primes)
+print "Length" + str(len(primes))
+print "Sum" + str(sum(primes))
